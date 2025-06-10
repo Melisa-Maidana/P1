9f5fd180246cd90dc7c8eb2c0d2020361b05da2c
Melisa Maidana <maidana.melisaanael@iesconcepcion.edu.ar>
2025-06-10 19:30:51 -0300

    modifique el index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const prompt = requiere('prompt-sync')
+const prompt = requiere('prompt-sync')()
 const limpiarPantalla = requiere ('clear')
 
+const nombre = prompt('Ingresa tu nombre:')
+const edad = Number(prompt('Ingresa tu edad:'))
+
+console.log('Hola',nombre, 'que lindo que tengas', edad, 'años')
+console.log('Hola',nombre, 'que lindo que tengas', edad, 'años')
+console.log('Hola',nombre, 'que lindo que tengas', edad, 'años')
+console.log('Hola',nombre, 'que lindo que tengas', edad, 'años')
+limpiarPantalla()
